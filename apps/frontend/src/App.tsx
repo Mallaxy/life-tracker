4fd3d7294005822591dcabcb5c0775910bd89713
Artem Dovhyi <77543336+Mallaxy@users.noreply.github.com>
2023-09-18 15:36:24 +0300

    Develop (#6)
    * configuration: default GitHub actions workflow edit
    
    * fix: add cache dependency path
    
    * fix: add job event
    
    * fix: fix script path
    
    * test: test actions
    
    * test: fix test actions
    
    * configuration: job on master push
    
    * improvement: add MUI to the project
    
    * improvement: add a setup guide
    
    * Test/start (#5)
    
    * updateTextInAPP
    
    * changeTextInApp
    
    ---------
    
    Co-authored-by: Artem Dovhyi <77543336+Mallaxy@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Maksym Boichenko <97298946+MaxBoichenko@users.noreply.github.com>

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { PageContainer } from './layout/PageContainer.tsx'
 import { Typography } from '@mui/material'
 export const App = () => {
-  const string = 'Hello world'
+  const string = 'Hello world + test+'
 
   return (
     <PageContainer>
